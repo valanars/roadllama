4b0d38b9b6d8b51ade84889547926630ed21aefc
Unknown <vl979@nyu.edu>
2017-04-30 15:52:50 -0400

    doin more menu stuff

--- Assets/Script/menus.cs
+++ Assets/Script/menus.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class menus : MonoBehaviour {
 
 
-	public bool start;
+	public bool Menu;
+	public bool Credits;
+	public bool Controls;
+
+	public bool playHover = true;
+	public bool creditHover = false;
+	public bool controlHover = false;
 
 
 
@@ -16,9 +22,36 @@ public class menus : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Vector3 currentPos = transform.position;
 
-		if (Input.GetKeyDown (KeyCode.Return)){
+
+		if (playHover == true && Input.GetKeyUp (KeyCode.LeftArrow)){
+			playHover = false;
+			creditHover = true;
+		} else if (playHover == false && creditHover == true && Input.GetKeyUp (KeyCode.RightArrow)){
+			playHover = true;
+			creditHover = false;
+		} else if (playHover == true && Input.GetKeyUp (KeyCode.RightArrow)){
+			playHover = false;
+			controlHover = true;
+		} else if (playHover == false && controlHover == true && Input.GetKeyUp (KeyCode.LeftArrow)){
+			playHover = true;
+			controlHover = false;
+		}
+
+
+			
+
+		transform.position = currentPos;
+
+		if (playHover == true && Input.GetKeyDown (KeyCode.Return)){
 			Application.LoadLevel ("Main Level");
+		} 
+		if (creditHover == true && Input.GetKeyDown (KeyCode.Return)){
+			Application.LoadLevel ("Credits");
+		}
+		if (controlHover == true && Input.GetKeyDown (KeyCode.Return)) {
+			Application.LoadLevel ("Controls");
 		}
 		
 	}
