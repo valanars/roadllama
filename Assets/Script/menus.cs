e20c4c3b0140827e6d55a211c8093ebded4dab0a
Unknown <vl979@nyu.edu>
2017-04-30 16:09:38 -0400

    tweaked real quick

--- Assets/Script/menus.cs
+++ Assets/Script/menus.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class menus : MonoBehaviour {
 
 
-	public bool Menu;
-	public bool Credits;
-	public bool Controls;
-
 	public bool playHover = true;
 	public bool creditHover = false;
 	public bool controlHover = false;
+
+	public Sprite[] playSelect;
+	public SpriteRenderer playButton;
+	public Sprite[] creditSelect;
+	public SpriteRenderer creditButton;
+	public Sprite[] controlSelect;
+	public SpriteRenderer controlButton;
 
 
 
 	// Use this for initialization
 	void Start () {
+		playButton.sprite = playSelect [1];
+		creditButton.sprite = creditSelect [0];
+		controlButton.sprite = controlSelect [0];
 		
 	}
 	
@@ -28,15 +34,27 @@ public class menus : MonoBehaviour {
 		if (playHover == true && Input.GetKeyUp (KeyCode.LeftArrow)){
 			playHover = false;
 			creditHover = true;
+			playButton.sprite = playSelect [0];
+			creditButton.sprite = creditSelect [1];
+			controlButton.sprite = controlSelect [0];
 		} else if (playHover == false && creditHover == true && Input.GetKeyUp (KeyCode.RightArrow)){
 			playHover = true;
 			creditHover = false;
+			playButton.sprite = playSelect [1];
+			creditButton.sprite = creditSelect [0];
+			controlButton.sprite = controlSelect [0];
 		} else if (playHover == true && Input.GetKeyUp (KeyCode.RightArrow)){
 			playHover = false;
 			controlHover = true;
+			playButton.sprite = playSelect [0];
+			creditButton.sprite = creditSelect [0];
+			controlButton.sprite = controlSelect [1];
 		} else if (playHover == false && controlHover == true && Input.GetKeyUp (KeyCode.LeftArrow)){
 			playHover = true;
 			controlHover = false;
+			playButton.sprite = playSelect [1];
+			creditButton.sprite = creditSelect [0];
+			controlButton.sprite = controlSelect [0];
 		}
 
 
